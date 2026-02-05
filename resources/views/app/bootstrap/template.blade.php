d965e75a80e7970db845d049f7809cf24f668276
ncerrod2606 <ncerrod2606@ieszaidinvergeles.org>
2026-02-05 13:11:18 +0000

    completo

--- resources/views/app/bootstrap/template.blade.php
+++ resources/views/app/bootstrap/template.blade.php
@@ -6,7 +6,7 @@
         <link rel="icon" type="image/x-icon" href="{{ url('assets/img/favicon.ico') }}">
         <!-- directiva -->
         <title>
-            @yield('title', 'Barber Shop')
+            @yield('title', 'Travel Agency App')
         </title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
